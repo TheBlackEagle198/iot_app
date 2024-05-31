13fceb20508b56fee42b3ed8401c23e4717c451f
Bogdan Plescan <and3bogdan198@gmail.com>
2024-05-31 16:35:43 +0300

    Add a Module class encapsulating common module functionality

---
+++ lib/Module/Module.cpp
@@ -0,0 +1,235 @@
+#include "Module.h"
+#include <SPI.h>
+#include <EEPROM.h>
+#include "RadioType.h"
+#include "Payload.h"
+
+Module::Module(GLOBAL_ID_T defaultGlobalId, uint8_t cePin, uint8_t csPin, uint8_t buttonPin) :
+    radio(cePin, csPin), network(radio), mesh(radio, network), connectButtonPin(buttonPin),
+    connectButtonTimer(1000), sendTimer(1000) {
+    globalId = this->readGIDFromEEPROM();
+    if (globalId == 0) {
+        globalId = defaultGlobalId;
+    }
+    Serial.println("Hello, world!");
+}
+
+GLOBAL_ID_T Module::readGIDFromEEPROM() {
+    GLOBAL_ID_T gid = 0;
+    EEPROM.get(0, gid);
+    return gid;
+}
+
+void Module::retryMeshConnection() {
+    do
+    {
+        Serial.print(F("Reconnecting to the mesh... Current address: "));
+        Serial.println(this->mesh.mesh_address, OCT);
+    } while (this->mesh.renewAddress() == MESH_DEFAULT_ADDRESS);
+}
+
+void Module::safeWriteToMesh(const void *data, uint8_t msg_type, size_t size, uint8_t recvNodeId) {
+    if (!mesh.write(data, msg_type, size, recvNodeId)) {
+        // If a write fails, check connectivity to the mesh network
+        if (!mesh.checkConnection()) {
+            retryMeshConnection();
+        }
+        else {
+            Serial.println("Send fail, Still connected to mesh");
+        }
+    }
+    else {
+        Serial.print("Send OK; self node id:");
+        Serial.print(mesh.getNodeID());
+        Serial.print("self mesh id:");
+        Serial.println(mesh.mesh_address, OCT);
+    }
+}
+
+bool Module::initRadio() {
+    // Set the nodeID manually to a temporary value
+    mesh.setNodeID(MESH_MAX_NODE_ID);
+
+    // Connect to the mesh
+    Serial.println("Connecting to the mesh...");
+    if (!mesh.begin(97, RF24_1MBPS, 2000)) {
+        Serial.println("Mesh failed to start.");
+        return false;
+    } else {
+        // change the temporary node id to the first available address
+        // this prevents collisions with other nodes that may be starting up
+        for (uint16_t nodeId = MESH_MAX_NODE_ID - 1; nodeId > 0; nodeId--) {
+            int currentMeshAddress = mesh.getAddress(nodeId);
+            if (currentMeshAddress <= 0) {
+                // found an unassigned address
+                mesh.setNodeID(nodeId);
+                retryMeshConnection();
+                break;
+            }
+            else {
+                Serial.print("Address ");
+                Serial.print(nodeId);
+                Serial.print(" is assigned to node ");
+                Serial.println(currentMeshAddress);
+            }
+        }
+
+        Serial.print("Recieved new temporary address: ");
+        Serial.println(mesh.mesh_address, OCT);
+    }
+
+    // not sure if this update is needed; added it just in case
+    mesh.update();
+
+    // perform the nodeID neogitation based on the GID
+    // initialize the negotiation
+    safeWriteToMesh((void*)&globalId, (uint8_t)RadioType::GID_NEGOTIATION, sizeof(GLOBAL_ID_T));
+
+    // wait for a response from the master node
+    Serial.print("Waiting for master node");
+    while (!network.available()) {
+        // Serial.print(".");
+        mesh.update();
+    }
+    Serial.println();
+    Serial.println("Done waiting for master node");
+
+    RF24NetworkHeader header;
+    network.peek(header);
+    if (header.type == (uint8_t)RadioType::GID_NEGOTIATION) {
+        uint16_t receivedId;
+        Serial.print("Master sent back a GID_NEGOTIATION message, read ");
+        Serial.print(network.read(header, &receivedId, sizeof(receivedId)));
+        Serial.println(" bytes");
+        Serial.print("Recieved NodeID: ");
+        Serial.println(receivedId);
+
+        mesh.releaseAddress();
+        mesh.setNodeID(receivedId);
+    } else {
+        Serial.println("Master sent back something else");
+        return false;
+    }
+    return true;
+}
+
+void Module::init() {
+    initSubmodule();
+    pinMode(connectButtonPin, INPUT_PULLUP);
+    currentState = ConnectionState::NOT_CONNECTED;
+}
+
+void Module::run() {
+    switch(currentState) {
+        case ConnectionState::CONNECTED:
+            if (!radioModuleAttached) {
+                switchState(ConnectionState::NOT_CONNECTED);
+                return;
+            }
+            mesh.update();
+            if (network.available()) {
+                RF24NetworkHeader header;
+                network.peek(header);
+                Serial.println(header.toString());
+            }
+            switch (sendStrategy) {
+                case SendStrategy::SEND_ON_CHANGE:
+                    if (shouldSend()) {
+                        sendData();
+                    }
+                    break;
+                case SendStrategy::SEND_ALWAYS:
+                    if (sendTimer.elapsed()) {
+                        sendData();
+                        sendTimer.reset();
+                    }
+                    break;
+            }
+            break;
+        case ConnectionState::NOT_CONNECTED:
+            if (!ranOnce) {
+                mesh.begin();
+                ranOnce = true;
+                if (!radio.isChipConnected()) {
+                    Serial.println("Radio hardware not responding.");
+                } else {
+                    radioModuleAttached = true;
+                }
+            }
+            break;
+        case ConnectionState::CONNECTING:
+            if (!radioModuleAttached) {
+                switchState(ConnectionState::NOT_CONNECTED);
+                return;
+            }
+            if (!ranOnce) {
+                ranOnce = true;
+                if (initRadio()) {
+                    switchState(ConnectionState::RETRY_CONNECTION);
+                } else {
+                    switchState(ConnectionState::NOT_CONNECTED);
+                }
+            }
+            break;
+        case ConnectionState::RETRY_CONNECTION:
+            if (!radioModuleAttached) {
+                switchState(ConnectionState::NOT_CONNECTED);
+                return;
+            }
+            if (!ranOnce) {
+                Serial.println("Retrying connection");
+                retryMeshConnection();
+                Serial.println("done");
+                ranOnce = true;
+                switchState(ConnectionState::CONNECTED);
+            }
+            break;
+    }
+    connectButtonState = digitalRead(connectButtonPin);
+    if (lastConnectButtonState == HIGH && connectButtonState == LOW) {
+        connectButtonTimer.reset();
+    } else if (connectButtonState == HIGH && lastConnectButtonState == LOW) {
+        if (connectButtonTimer.elapsed()) {
+            Serial.println("Long press detected");
+            switchState(ConnectionState::CONNECTING);
+        }
+    }
+    lastConnectButtonState = connectButtonState;
+}
+
+void Module::switchState(ConnectionState newState) {
+    Serial.print("Switching state to ");
+    Serial.println(stateToStr(newState));
+    ranOnce = false;
+    currentState = newState;
+}
+
+inline const char* Module::stateToStr(ConnectionState state) {
+    switch (state) {
+    case ConnectionState::CONNECTED:
+        return "CONNECTED";
+    case ConnectionState::NOT_CONNECTED:
+        return "NOT_CONNECTED";
+    case ConnectionState::CONNECTING:
+        return "CONNECTING";
+    case ConnectionState::RETRY_CONNECTION:
+        return "RETRY_CONNECTION";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+void Module::initSendStrategy(SendStrategy strategy) {
+    switch (strategy) {
+        case SendStrategy::SEND_ON_CHANGE:
+            break;
+        case SendStrategy::SEND_ALWAYS:
+            sendTimer.reset();
+            break;
+    }
+}
+
+void Module::changeStrategy(SendStrategy strategy) {
+    initSendStrategy(strategy);
+    sendStrategy = strategy;
+}
--- lib/Module/Module.h
+++ lib/Module/Module.h
@@ -1,4 +1,94 @@
 #pragma once
+#include <stdint.h>
+#include <RF24.h>
+#include <RF24Network.h>
+#include <RF24Mesh.h>
+#include "GlobalConfig.h"
+#include "Timer.h"
+
+#define DEFAULT_INTERVAL 1000 ///< default interval for sending updates to the master node
+
+#define MESH_MASTER_NODE_ID 0 ///< The ID of the master node
+#define MESH_MAX_NODE_ID 255 ///< The maximum node ID
+
+/// related to the mesh library; added just in case
+#ifdef MESH_NOMASTER
+#undef MESH_NOMASTER
+#endif
+
+enum ConnectionState {
+    CONNECTED,
+    NOT_CONNECTED,
+    CONNECTING,
+    RETRY_CONNECTION
+};
+
+enum class SendStrategy {
+    SEND_ON_CHANGE,
+    SEND_ALWAYS
+};
 
 class Module {
+protected:
+    RF24 radio;
+    RF24Network network;
+    RF24Mesh mesh;
+    GLOBAL_ID_T globalId;
+
+    bool radioModuleAttached = false;
+
+    // stuff for the button
+    uint8_t connectButtonState;
+    uint8_t lastConnectButtonState;
+    uint8_t connectButtonPin;
+    Timer connectButtonTimer;
+
+    // stuff for sending data
+    SendStrategy sendStrategy = SendStrategy::SEND_ON_CHANGE;
+    Timer sendTimer;
+    ConnectionState currentState = NOT_CONNECTED;
+    bool ranOnce = false;
+public:
+    Module(GLOBAL_ID_T defaultGlobalId, uint8_t cePin, uint8_t csPin, uint8_t buttonPin);
+
+    virtual void initSubmodule() = 0;
+
+    // initializes the module
+    void init();
+
+    // main function of the module; should be called periodically
+    void run();
+
+    /// @brief retrieves the global id from EEPROM
+    /// @return the global id read from EEPROM
+    GLOBAL_ID_T readGIDFromEEPROM();
+
+    /// @brief Retries the mesh connection until it succeeds
+    void retryMeshConnection();
+
+    /// @brief Safely writes to the mesh, checking for connection and retrying if necessary
+    void safeWriteToMesh(const void *data, uint8_t msg_type, size_t size, uint8_t recvNodeId = (uint8_t)0U);
+
+    // @brief inititlizes the radio connection by performing gid negotiation
+    // @return true if the radio connection was successful, false otherwise
+    bool initRadio();
+
+    // @brief converts a connection state to a string
+    // @return the string representation of the connection state
+    inline const char* stateToStr(ConnectionState state);
+
+    // @brief switches the connection state of the module
+    void switchState(ConnectionState newState);
+
+    // initializes the send strategy
+    void initSendStrategy(SendStrategy strategy);
+
+    // changes the send strategy; DON'T switch to any states in this method
+    void changeStrategy(SendStrategy strategy);
+
+    // @brief checks if the module should send data i.e. if the threshodls are met
+    virtual bool shouldSend() = 0;
+
+    // @brief sends the data to the master node
+    virtual void sendData() = 0;
 };
