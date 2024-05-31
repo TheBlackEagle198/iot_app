d621de843afda19045e2846728b23f94e9ee6281
Bogdan Plescan <and3bogdan198@gmail.com>
2024-05-31 16:34:15 +0300

    Add timer class for async operations

---
+++ lib/Timer/Timer.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include "Timer.h"
+
+void Timer::setInterval(uint32_t interval) {
+    this->interval = interval;
+}
+
+void Timer::reset() {
+    lastTime = millis();
+}
+
+bool Timer::elapsed() {
+    if (millis() - lastTime >= interval) {
+        return true;
+    }
+    return false;
+}
---
+++ lib/Timer/Timer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class Timer {
+private:
+    uint32_t interval;
+    uint32_t lastTime;
+public:
+    Timer(uint32_t interval) : interval(interval) {}
+    
+    // @brief sets the interval of the timer
+    void setInterval(uint32_t interval);
+    
+    // @brief resets the timer
+    void reset();
+    
+    // @brief checks if the timer has elapsed
+    // @return true if the timer has elapsed, false otherwise
+    bool elapsed();
+};
